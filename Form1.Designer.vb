e996126d3d63fe29d347f8005b3a6d1fff880869
squawki <contact@jamesfranklin.au>
2024-03-26 09:04:15 +1000

    Version 0.1 of SQLite Backup. Mostly fully functional, but have a few things i want to add before final release

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,10 +22,298 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        components = New ComponentModel.Container()
+        txtSourceLocation = New TextBox()
+        lblSource = New Label()
+        Label1 = New Label()
+        txtBackupLocation = New TextBox()
+        chk_appendTimeStamp = New CheckBox()
+        btnBackupNow = New Button()
+        btnSaveConfig = New Button()
+        Panel1 = New Panel()
+        lblNextBackupDisplay = New Label()
+        Label5 = New Label()
+        pnl_automatedIndicator = New Panel()
+        btn_automatedEnabled = New Button()
+        chk_automated_runStart = New CheckBox()
+        Label3 = New Label()
+        lblInterval = New Label()
+        numeric_automatedInterval = New NumericUpDown()
+        Label2 = New Label()
+        Label4 = New Label()
+        Timer1 = New Timer(components)
+        radio_vacumm = New RadioButton()
+        radio_filecopy = New RadioButton()
+        Label6 = New Label()
+        Panel1.SuspendLayout()
+        CType(numeric_automatedInterval, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' txtSourceLocation
+        ' 
+        txtSourceLocation.Font = New Font("Segoe UI", 9F)
+        txtSourceLocation.Location = New Point(47, 62)
+        txtSourceLocation.Name = "txtSourceLocation"
+        txtSourceLocation.Size = New Size(657, 23)
+        txtSourceLocation.TabIndex = 0
+        ' 
+        ' lblSource
+        ' 
+        lblSource.AutoSize = True
+        lblSource.Font = New Font("Segoe UI", 14F)
+        lblSource.Location = New Point(47, 34)
+        lblSource.Name = "lblSource"
+        lblSource.Size = New Size(108, 25)
+        lblSource.TabIndex = 1
+        lblSource.Text = "Source File:"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 14F)
+        Label1.Location = New Point(42, 89)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(154, 25)
+        Label1.TabIndex = 3
+        Label1.Text = "Backup Location:"
+        ' 
+        ' txtBackupLocation
+        ' 
+        txtBackupLocation.Font = New Font("Segoe UI", 9F)
+        txtBackupLocation.Location = New Point(48, 117)
+        txtBackupLocation.Name = "txtBackupLocation"
+        txtBackupLocation.Size = New Size(657, 23)
+        txtBackupLocation.TabIndex = 2
+        ' 
+        ' chk_appendTimeStamp
+        ' 
+        chk_appendTimeStamp.AutoSize = True
+        chk_appendTimeStamp.Checked = True
+        chk_appendTimeStamp.CheckState = CheckState.Checked
+        chk_appendTimeStamp.Font = New Font("Segoe UI", 12F)
+        chk_appendTimeStamp.Location = New Point(48, 157)
+        chk_appendTimeStamp.Name = "chk_appendTimeStamp"
+        chk_appendTimeStamp.Size = New Size(283, 25)
+        chk_appendTimeStamp.TabIndex = 4
+        chk_appendTimeStamp.Text = "Append Timestamp (Recommended)"
+        chk_appendTimeStamp.UseVisualStyleBackColor = True
+        ' 
+        ' btnBackupNow
+        ' 
+        btnBackupNow.Location = New Point(47, 297)
+        btnBackupNow.Name = "btnBackupNow"
+        btnBackupNow.Size = New Size(170, 37)
+        btnBackupNow.TabIndex = 5
+        btnBackupNow.Text = "Backup Now"
+        btnBackupNow.UseVisualStyleBackColor = True
+        ' 
+        ' btnSaveConfig
+        ' 
+        btnSaveConfig.Location = New Point(47, 343)
+        btnSaveConfig.Name = "btnSaveConfig"
+        btnSaveConfig.Size = New Size(170, 37)
+        btnSaveConfig.TabIndex = 6
+        btnSaveConfig.Text = "Save Config"
+        btnSaveConfig.UseVisualStyleBackColor = True
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.LemonChiffon
+        Panel1.Controls.Add(lblNextBackupDisplay)
+        Panel1.Controls.Add(Label5)
+        Panel1.Controls.Add(pnl_automatedIndicator)
+        Panel1.Controls.Add(btn_automatedEnabled)
+        Panel1.Controls.Add(chk_automated_runStart)
+        Panel1.Controls.Add(Label3)
+        Panel1.Controls.Add(lblInterval)
+        Panel1.Controls.Add(numeric_automatedInterval)
+        Panel1.Controls.Add(Label2)
+        Panel1.Location = New Point(480, 182)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(225, 146)
+        Panel1.TabIndex = 7
+        ' 
+        ' lblNextBackupDisplay
+        ' 
+        lblNextBackupDisplay.AutoSize = True
+        lblNextBackupDisplay.Location = New Point(103, 126)
+        lblNextBackupDisplay.Name = "lblNextBackupDisplay"
+        lblNextBackupDisplay.Size = New Size(17, 15)
+        lblNextBackupDisplay.TabIndex = 12
+        lblNextBackupDisplay.Text = "--"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(24, 126)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(80, 15)
+        Label5.TabIndex = 11
+        Label5.Text = "Next Backup: "
+        ' 
+        ' pnl_automatedIndicator
+        ' 
+        pnl_automatedIndicator.BackColor = Color.White
+        pnl_automatedIndicator.Location = New Point(164, 94)
+        pnl_automatedIndicator.Name = "pnl_automatedIndicator"
+        pnl_automatedIndicator.Size = New Size(19, 21)
+        pnl_automatedIndicator.TabIndex = 9
+        ' 
+        ' btn_automatedEnabled
+        ' 
+        btn_automatedEnabled.FlatStyle = FlatStyle.Flat
+        btn_automatedEnabled.Location = New Point(25, 92)
+        btn_automatedEnabled.Name = "btn_automatedEnabled"
+        btn_automatedEnabled.Size = New Size(133, 25)
+        btn_automatedEnabled.TabIndex = 8
+        btn_automatedEnabled.Text = "Disabled"
+        btn_automatedEnabled.UseVisualStyleBackColor = True
+        ' 
+        ' chk_automated_runStart
+        ' 
+        chk_automated_runStart.AutoSize = True
+        chk_automated_runStart.Font = New Font("Segoe UI", 9F)
+        chk_automated_runStart.Location = New Point(25, 67)
+        chk_automated_runStart.Name = "chk_automated_runStart"
+        chk_automated_runStart.Size = New Size(140, 19)
+        chk_automated_runStart.TabIndex = 8
+        chk_automated_runStart.Text = "Run on Program Start"
+        chk_automated_runStart.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(148, 46)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(50, 15)
+        Label3.TabIndex = 10
+        Label3.Text = "Minutes"
+        ' 
+        ' lblInterval
+        ' 
+        lblInterval.AutoSize = True
+        lblInterval.Location = New Point(25, 44)
+        lblInterval.Name = "lblInterval"
+        lblInterval.Size = New Size(49, 15)
+        lblInterval.TabIndex = 8
+        lblInterval.Text = "Interval:"
+        ' 
+        ' numeric_automatedInterval
+        ' 
+        numeric_automatedInterval.Location = New Point(80, 38)
+        numeric_automatedInterval.Maximum = New Decimal(New Integer() {9999, 0, 0, 0})
+        numeric_automatedInterval.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        numeric_automatedInterval.Name = "numeric_automatedInterval"
+        numeric_automatedInterval.Size = New Size(62, 23)
+        numeric_automatedInterval.TabIndex = 9
+        numeric_automatedInterval.Value = New Decimal(New Integer() {60, 0, 0, 0})
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 14F)
+        Label2.Location = New Point(16, 10)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(193, 25)
+        Label2.TabIndex = 8
+        Label2.Text = "Backup Automatically"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 16F, FontStyle.Bold)
+        Label4.Location = New Point(253, 9)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(214, 30)
+        Label4.TabIndex = 8
+        Label4.Text = "Easy SQLite Backup"
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Enabled = True
+        Timer1.Interval = 1000
+        ' 
+        ' radio_vacumm
+        ' 
+        radio_vacumm.AutoSize = True
+        radio_vacumm.Checked = True
+        radio_vacumm.Font = New Font("Segoe UI", 10F)
+        radio_vacumm.Location = New Point(47, 229)
+        radio_vacumm.Name = "radio_vacumm"
+        radio_vacumm.Size = New Size(124, 23)
+        radio_vacumm.TabIndex = 9
+        radio_vacumm.TabStop = True
+        radio_vacumm.Text = "VACUMM INTO"
+        radio_vacumm.UseVisualStyleBackColor = True
+        ' 
+        ' radio_filecopy
+        ' 
+        radio_filecopy.AutoSize = True
+        radio_filecopy.Font = New Font("Segoe UI", 10F)
+        radio_filecopy.Location = New Point(48, 258)
+        radio_filecopy.Name = "radio_filecopy"
+        radio_filecopy.Size = New Size(92, 23)
+        radio_filecopy.TabIndex = 11
+        radio_filecopy.TabStop = True
+        radio_filecopy.Text = "FILE COPY"
+        radio_filecopy.UseVisualStyleBackColor = True
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 14F)
+        Label6.Location = New Point(42, 201)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(131, 25)
+        Label6.TabIndex = 12
+        Label6.Text = "Backup Mode:"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(751, 394)
+        Controls.Add(Label6)
+        Controls.Add(radio_filecopy)
+        Controls.Add(radio_vacumm)
+        Controls.Add(Label4)
+        Controls.Add(Panel1)
+        Controls.Add(btnSaveConfig)
+        Controls.Add(btnBackupNow)
+        Controls.Add(chk_appendTimeStamp)
+        Controls.Add(Label1)
+        Controls.Add(txtBackupLocation)
+        Controls.Add(lblSource)
+        Controls.Add(txtSourceLocation)
+        Name = "Form1"
+        Text = "Easy SQLite Backup"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        CType(numeric_automatedInterval, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+    Friend WithEvents txtSourceLocation As TextBox
+    Friend WithEvents lblSource As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtBackupLocation As TextBox
+    Friend WithEvents chk_appendTimeStamp As CheckBox
+    Friend WithEvents btnBackupNow As Button
+    Friend WithEvents btnSaveConfig As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents chk_automated_runStart As CheckBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblInterval As Label
+    Friend WithEvents numeric_automatedInterval As NumericUpDown
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btn_automatedEnabled As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents pnl_automatedIndicator As Panel
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents lblNextBackupDisplay As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents radio_vacumm As RadioButton
+    Friend WithEvents radio_filecopy As RadioButton
+    Friend WithEvents Label6 As Label
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,301 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports System.Data.SQLite
+
+Imports System.Runtime.InteropServices
+Imports System.Reflection
+Imports System.IO.Enumeration
+Public Class Form1
+    Private configFilePath As String = "config.ini"
+    Dim backup_enabled As Boolean
+    Dim backup_nextbackupseconds As Integer = 9999
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadConfiguration()
+    End Sub
+
+    Private Sub BackupDatabase(ByVal sourcelocation As String, ByVal backuplocation As String, ByVal silent As Boolean, ByVal appendtimestamp As Boolean, ByVal writelog As Boolean)
+
+        Dim _backuplocation As String = backuplocation
+
+
+
+        If appendtimestamp = True Then
+
+            Dim directoryPath As String = Path.GetDirectoryName(_backuplocation)
+            Dim fileName As String = Path.GetFileNameWithoutExtension(_backuplocation)
+            Dim fileExtension As String = Path.GetExtension(_backuplocation)
+
+            ' Generate timestamp
+            Dim timestamp As String = DateTime.Now.ToString("ddMMyyyy_HHmmss")
+
+            ' Append timestamp before the extension
+            Dim newFileName As String = fileName & "_" & timestamp & fileExtension
+
+            ' Form the new directory string
+            Dim newDirectoryString As String = Path.Combine(directoryPath, newFileName)
+
+            _backuplocation = newDirectoryString
+        End If
+
+
+
+        If radio_filecopy.Checked Then
+
+            '  Offline Copy Method
+            Try
+                ' Check if the source file exists
+                If File.Exists(sourcelocation) Then
+                    ' Copy the source file to the backup destination
+                    File.Copy(sourcelocation, _backuplocation, True)
+                    If silent = False Then
+                        MessageBox.Show("Backup completed successfully.", "Backup", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    End If
+                Else
+                    If silent = False Then
+                        MessageBox.Show("Source database file does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    End If
+                End If
+            Catch ex As Exception
+                If silent = False Then
+                    MessageBox.Show("Error occurred: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End If
+            End Try
+        End If
+
+
+        If radio_vacumm.Checked Then
+
+            'Vacumm SQLiteMethod
+            Try
+                ' Check if the source file exists
+                If File.Exists(sourcelocation) Then
+                    ' Connect to the source database
+                    Using sourceConnection As New SQLiteConnection($"Data Source={sourcelocation}")
+                        sourceConnection.Open()
+
+                        ' Execute VACUUM command to optimize and clean up the source database
+                        Using cmd As New SQLiteCommand("VACUUM", sourceConnection)
+                            cmd.ExecuteNonQuery()
+
+                        End Using
+                    End Using
+
+                    ' Copy the optimized source database to the backup destination
+                    File.Copy(sourcelocation, _backuplocation, True)
+
+                    If Not silent Then
+                        MessageBox.Show("Backup completed successfully.", "Backup", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    End If
+                Else
+                    If Not silent Then
+                        MessageBox.Show("Source database file does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    End If
+                End If
+            Catch ex As Exception
+                If Not silent Then
+                    MessageBox.Show("Error occurred: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End If
+            End Try
+
+        End If
+
+
+    End Sub
+
+    Private Sub txtSourceLocation_Click(sender As Object, e As EventArgs) Handles txtSourceLocation.DoubleClick
+        ' Display the OpenFileDialog to select the source database file
+        Dim openFileDialog As New OpenFileDialog()
+        openFileDialog.Filter = "SQLite Database File (*.db)|*.db"
+        openFileDialog.Title = "Select Source Database File"
+        openFileDialog.InitialDirectory = "C:\"
+        openFileDialog.RestoreDirectory = True
+
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            txtSourceLocation.Text = openFileDialog.FileName
+        End If
+    End Sub
+
+    Private Sub txtBackupLocation_Click(sender As Object, e As EventArgs) Handles txtBackupLocation.DoubleClick
+        Dim saveFileDialog As New SaveFileDialog()
+        saveFileDialog.Filter = "SQLite Database File (*.db)|*.db"
+        saveFileDialog.Title = "Select Backup Destination File"
+        saveFileDialog.InitialDirectory = "C:\"
+        saveFileDialog.RestoreDirectory = True
+
+        If saveFileDialog.ShowDialog() = DialogResult.OK Then
+            txtBackupLocation.Text = saveFileDialog.FileName
+        End If
+    End Sub
+
+    Private Sub btnBackupNow_Click(sender As Object, e As EventArgs) Handles btnBackupNow.Click
+
+        BackupDatabase(txtSourceLocation.Text, txtBackupLocation.Text, False, chk_appendTimeStamp.Checked, False)
+
+    End Sub
+
+    Private Sub SaveConfiguration()
+
+        Dim backupmode As String = ""
+        If radio_vacumm.Checked Then backupmode = "VACUUM"
+        If radio_filecopy.Checked Then backupmode = "FILECOPY"
+        Try
+            ' Create a StreamWriter to write to the config file
+            Using writer As New StreamWriter(configFilePath)
+                ' Write the source file path and backup folder path to the config file
+                writer.WriteLine("SourceFilePath=" & txtSourceLocation.Text)
+                writer.WriteLine("BackupFolderPath=" & txtBackupLocation.Text)
+                writer.WriteLine("AppendTimestamp=" & chk_appendTimeStamp.Checked)
+                writer.WriteLine("AutomatedInterval=" & numeric_automatedInterval.Value)
+                writer.WriteLine("AutomatedAutoRun=" & chk_automated_runStart.Checked)
+                writer.WriteLine("BackupMode=" & backupmode)
+
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Error occurred while saving configuration: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+    Private Sub LoadConfiguration()
+        Dim backupmode As String = ""
+
+        If File.Exists(configFilePath) Then
+            Try
+                ' Read configuration from the config file
+                Dim lines As String() = File.ReadAllLines(configFilePath)
+                For Each line As String In lines
+                    Dim parts As String() = line.Split("="c)
+                    If parts.Length = 2 Then
+                        Dim key As String = parts(0)
+                        Dim value As String = parts(1)
+                        If key = "SourceFilePath" Then
+                            txtSourceLocation.Text = value
+                        ElseIf key = "BackupFolderPath" Then
+                            txtBackupLocation.Text = value
+                        ElseIf key = "AppendTimestamp" Then
+                            chk_appendTimeStamp.Checked = value
+                        ElseIf key = "AutomatedInterval" Then
+                            numeric_automatedInterval.Value = value
+                        ElseIf key = "AutomatedAutoRun" Then
+                            chk_automated_runStart.Checked = value
+                        ElseIf key = "BackupMode" Then
+                            backupmode = value
+
+                        End If
+                    End If
+                Next
+                If chk_automated_runStart.Checked = False Then
+                    backup_enabled = False
+                    btn_automatedEnabled.Text = "Disabled"
+                    btn_automatedEnabled.BackColor = Color.LightGray
+
+                Else
+                    backup_enabled = True
+                    btn_automatedEnabled.Text = "Enabled"
+                    btn_automatedEnabled.BackColor = Color.LightGreen
+
+                End If
+
+                Select Case backupmode
+                    Case "VACUUM"
+                        radio_vacumm.Checked = True
+                    Case "FILECOPY"
+                        radio_filecopy.Checked = True
+                    Case Else
+                        radio_vacumm.Checked = True
+                End Select
+
+
+            Catch ex As Exception
+                MessageBox.Show("Error occurred while loading configuration: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        Else
+            btn_automatedEnabled.Text = "Disabled"
+            backup_enabled = False
+            calculateNextBackup()
+        End If
+    End Sub
+
+    Private Sub btnSaveConfig_Click(sender As Object, e As EventArgs) Handles btnSaveConfig.Click
+        SaveConfiguration()
+    End Sub
+
+    Private Sub btn_automatedEnabled_Click(sender As Object, e As EventArgs) Handles btn_automatedEnabled.Click
+        Select Case btn_automatedEnabled.Text
+            Case "Enabled"
+                backup_enabled = False
+                btn_automatedEnabled.Text = "Disabled"
+                btn_automatedEnabled.BackColor = Color.LightGray
+            Case "Disabled"
+                backup_enabled = True
+                btn_automatedEnabled.Text = "Enabled"
+                btn_automatedEnabled.BackColor = Color.LightGreen
+                backup_nextbackupseconds = numeric_automatedInterval.Value * 60
+        End Select
+        calculateNextBackup()
+    End Sub
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        calculateNextBackup()
+
+        If backup_enabled = True Then
+            Select Case pnl_automatedIndicator.BackColor
+                Case Color.White
+                    pnl_automatedIndicator.BackColor = Color.LightGreen
+                Case Color.LightGreen
+                    pnl_automatedIndicator.BackColor = Color.White
+                Case Color.Purple
+                    pnl_automatedIndicator.BackColor = Color.White
+            End Select
+            backup_nextbackupseconds = backup_nextbackupseconds - 1
+
+            If backup_nextbackupseconds < 0 Then
+                pnl_automatedIndicator.BackColor = Color.Purple
+
+
+                BackupDatabase(txtSourceLocation.Text, txtBackupLocation.Text, True, chk_appendTimeStamp.Checked, False)
+
+                ResetBackup()
+            End If
+
+        Else
+            pnl_automatedIndicator.BackColor = Color.White
+
+
+
+        End If
+
+    End Sub
+    Private Sub calculateNextBackup()
+
+
+        If backup_enabled = True Then
+            Dim hours As Integer = backup_nextbackupseconds \ 3600
+            Dim minutes As Integer = (backup_nextbackupseconds Mod 3600) \ 60
+            Dim seconds As Integer = backup_nextbackupseconds Mod 60
+
+            Dim formattedTime As String = String.Format("{0:D2}:{1:D2}:{2:D2}", hours, minutes, seconds)
+            lblNextBackupDisplay.Text = formattedTime
+
+        Else
+            lblNextBackupDisplay.Text = "Never"
+        End If
+
+    End Sub
+
+    Private Sub numeric_automatedInterval_ValueChanged(sender As Object, e As EventArgs) Handles numeric_automatedInterval.ValueChanged
+        ResetBackup()
+
+    End Sub
+    Private Sub ResetBackup()
+        backup_nextbackupseconds = numeric_automatedInterval.Value * 60
+        calculateNextBackup()
+    End Sub
+
+    Private Sub numeric_automatedInterval_KeyUp(sender As Object, e As KeyEventArgs) Handles numeric_automatedInterval.KeyUp
+        ResetBackup()
+
+    End Sub
 
 End Class
+
